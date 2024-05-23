a84a05906ac035225103d34a76b5703c4220309c
Ajithan Arulgnanaselvam <155744696+mieszkoajithan@users.noreply.github.com>
2024-05-23 10:32:58 +0100

    tes

--- src/App.js
+++ src/App.js
@@ -28,17 +28,7 @@ const App = () => {
   const storedTheme = useSelector((state) => state.theme)
 
   useEffect(() => {
-
-
-
-
-
-
-
     const urlParams = new URLSearchParams(window.location.href.split('?')[1])
-
-
-    
     const theme = urlParams.get('theme') && urlParams.get('theme').match(/^[A-Za-z0-9\s]+/)[0]
     if (theme) {
       setColorMode(theme)
