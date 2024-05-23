5b823efaf49c0ea73bfb292ac05592c070cc2096
Ajithan Arulgnanaselvam <155744696+mieszkoajithan@users.noreply.github.com>
2024-05-23 10:28:25 +0100

    New

--- src/App.js
+++ src/App.js
@@ -33,9 +33,12 @@ const App = () => {
 
 
 
-    
+
 
     const urlParams = new URLSearchParams(window.location.href.split('?')[1])
+
+
+    
     const theme = urlParams.get('theme') && urlParams.get('theme').match(/^[A-Za-z0-9\s]+/)[0]
     if (theme) {
       setColorMode(theme)
